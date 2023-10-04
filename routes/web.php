405c6c8ea953264a42a7c20adefc51d410de7062
Habib Adama DIoumanera <habibadamadioumanera@gmail.com>
2023-10-04 20:18:23 +0000

    Ajout du projet sur gestion des taches en laravel

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::resource('property', \App\Http\Controllers\Admin\PropertyController::class)->except(['show']);
+    Route::resource('option',  \App\Http\Controllers\Admin\OptionController::class)->except(['show']);
+});
